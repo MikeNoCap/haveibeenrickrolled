6dcd6a7dcde536832e29b3d894a660c32dc2c0ac
Mike <thorvaldsenmikkel@gmail.com>
2021-07-02 18:04:59 +0200

    NEIGHBOOR

--- isthisarickroll/views.py
+++ isthisarickroll/views.py
@@ -15,7 +15,7 @@ class EmptyInput(Exception):
     """Raised when a form is submitted without any content."""
     pass
 
-with open('rick_db.pkl', 'rb') as fp:
+with open('isthisarickroll/rick_db.pkl', 'rb') as fp:
     ricklinks = pickle.load(fp)
     
 
@@ -171,7 +171,7 @@ def report_link(request):
                return render(request, 'report.html', {"message": "Link already in blacklist!"}) 
             
             ricklinks.append(screenname)
-            with open("rick_db.pkl", "wb") as fp:
+            with open("isthisarickroll/rick_db.pkl", "wb") as fp:
                 pickle.dump(ricklinks, fp)
 
             return render(request, 'report.html', {"message": "Added to blacklist!"}) 
